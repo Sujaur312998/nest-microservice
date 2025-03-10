0a645c38c8e55707aafc0dd6ebbe934947caa664
Sujaur312998 <suja312998@gmail.com>
2025-03-10 22:05:15 +0600

    books microservice added on monorepo

---
+++ apps/books/src/books/books.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+@Injectable()
+export class BooksService {
+  private books=[
+    {
+      id:1,
+      title:'Book 1',
+      author:'Author 1',
+      price:100
+    },
+    {
+      id:2,
+      title:'Book 2',
+      author:'Author 2',
+      price:200
+    }
+  ]
+  create(createBookDto: CreateBookDto) {
+    this.books.push(createBookDto)
+    return  this.books
+  }
+
+  findAll() {
+    return  this.books
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
